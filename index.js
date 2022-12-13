53a1f1036c9670ed42e14b672ddfb88c7552d792
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-12-12 21:02:43 -0800

    apple

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const canvas = document.querySelector("#snake-game")
 const ctx = canvas.getContext("2d") 
 
 // starts at (20, 20)
+let speed = 7
 let tileCount = 20
 let tileSize = 18
 let x = 10
@@ -13,7 +14,8 @@ function drawGame(){
 
     clearScreen()
     drawSnake()
-
+    moveSnake()
+    drawApple()
     // let speed = 7
     // setTimeout(drawGame, 1000/speed) // update screen 7x a second
     
@@ -34,32 +36,57 @@ function clearScreen(){ // fill with black
 let xSpeed = 0
 let ySpeed = 0
 
-document.addEventListener("keydown", keydown)
+document.body.addEventListener("keydown", keydown)
 
 function keydown(e){
     //up
     if(e.keyCode==38){
+        if(ySpeed == 1) return;
         ySpeed = -1 // one tile up
         xSpeed = 0
+        console.log("up")
     }
     
     //down
     if(e.keyCode==40){
+        if(ySpeed == -1) return;
         ySpeed = 1 // one tile down
         xSpeed = 0
+        console.log("down")
     }
     
     //left
     if(e.keyCode==37){
+        if(xSpeed == 1) return;
         ySpeed = 0
         xSpeed = -1 // one tile left
+        console.log("left")
     }
     
     //right
     if(e.keyCode==39){
+        if(ySpeed == -1) return;
         ySpeed = 0
         xSpeed = 1 // one tile right
+        console.log("right")
     }
 }
+
+function moveSnake(){
+    x = x + xSpeed
+    y = y + ySpeed
+}
+
+
+// apple variables
+
+let appleX = 5
+let appleY = 5
+
+function drawApple(){
+    ctx.fillStyle = "red"
+    ctx.fillRect(appleX * tileCount, appleY * tileCount, tileSize, tileSize)
+}
+
 
 drawGame()
