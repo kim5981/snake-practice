15d1d1cfc710a294fbc3e26433d5333134a85515
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-12-14 16:31:44 -0800

    debug move + add score

--- index.js
+++ index.js
@@ -3,40 +3,76 @@ const canvas = document.querySelector("#snake-game")
 // canvas 2d context -- drawn in 2d space
 const ctx = canvas.getContext("2d") 
 
+
+class snakeBit{
+    constructor(x, y){
+        this.x = x
+        this.y = y
+    }
+}
+
 // starts at (20, 20)
 let speed = 7
 let tileCount = 20
+
 let tileSize = 18
-let x = 10
-let y = 10
+let headX = 10
+let headY = 10
+
+//snake body array
+const snakeBits = []
+let tailLength = 2
+
+let xSpeed = 0
+let ySpeed = 0
+
+
+let appleX = 5
+let appleY = 5
+
+let score = 0
 
 function drawGame(){
 
+    moveSnake()
+
+    // game over
+
     clearScreen()
     drawSnake()
-    moveSnake()
     drawApple()
+
     checkCollision()
-    // let speed = 7
-    // setTimeout(drawGame, 1000/speed) // update screen 7x a second
+    setScore()
+    setTimeout(drawGame, 1000/speed) // update screen 7x a second
     
 }
 
 
 function drawSnake(){
     ctx.fillStyle = "white"
-    ctx.fillRect(x * tileCount, y * tileCount, tileSize, tileSize)
+
+    for(let i=0; i < snakeBits.length; i++){
+        let bit = snakeBits[i]
+        ctx.fillRect(bit.x * tileCount, bit.y * tileCount, tileSize, tileSize)
+    }
+        snakeBits.push(new snakeBit(headX, headY))
 }
 
-const snakeBits = []
-let tailLength = 2
+
 
 function checkCollision(){
-    if(appleX == x && appleY == y){
+    if(appleX == headX && appleY == headY){
         appleX = Math.floor(Math.random() * tileCount)
         appleY = Math.floor(Math.random() * tileCount)
         tailLength++
     }
+}
+
+function setScore(){
+    ctx.fillStyle = "white"
+    ctx.font = "14px verdena"
+    ctx.fillText("Score: " + score, canvas.clientWidth-50, 10)
 }
 
 function clearScreen(){ // fill with black
@@ -45,8 +81,7 @@ function clearScreen(){ // fill with black
 }
 
 // change snake direction
-let xSpeed = 0
-let ySpeed = 0
+
 
 document.body.addEventListener("keydown", keydown)
 
@@ -85,15 +120,13 @@ function keydown(e){
 }
 
 function moveSnake(){
-    x = x + xSpeed
-    y = y + ySpeed
+    headX = headX + xSpeed
+    headY = headY + ySpeed
 }
 
 
 // apple variables
 
-let appleX = 5
-let appleY = 5
 
 function drawApple(){
     ctx.fillStyle = "red"
