a4d88dd376144040dc8f762ff99c5de1b3820c96
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-12-14 18:55:17 -0800

    server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,23 @@
-const server = require("express")()
+const express = require("express")
+
+const server = express()
+const parser = require("body-parser")
+const cors = require("cors")
+
 const PORT = process.env.PORT || 5500
 
+server.use(parser.json())
+
+server.use(parser.urlencoded{
+    extended: true
+})
+
+server.use(cors())
+
 server.get("", (req, res) => {
-    res.send("server running at port", PORT)
+    res.send("welcome", PORT)
+})
+
+server.listen(port, () => {
+    console.log(`server running at port ${PORT}`)
 })
